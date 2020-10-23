f8e589a3aa675d689b828504e2c65d6d490682ec
ottagit <ottacc66@gmail.com>
2020-10-23 12:22:36 +0300

    implement code functionality using a function
    Organize your code into a set of statements within
    a function to make it (your code) more dynamic.

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Basic arithmetic
+  let name1 = "Kennedy";
+  let name2 = "Oduor";
+  // Use template literals (``) to join strings
+  let statement = `My name is ${name1} ${name2}`;
+  console.log(statement);
+*/
+
+// Implement above code using in a function declaration
+function personName(name1, name2) {
+  let statement = `My name is ${name1} ${name2}`;
+  console.log(statement);
+}
+// Call or invoke the function
+personName("Dorothy", "Akoth");
